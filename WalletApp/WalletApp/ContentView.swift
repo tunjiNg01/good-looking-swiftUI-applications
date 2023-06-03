6443a13363213e848e7cc02f23eee0286252a57f
tunjiNg01 <nahtlosng@gmail.com>
2023-06-03 10:27:24 +0100

    init commit

---
+++ WalletApp/WalletApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WalletApp
+//
+//  Created by MACBOOK PRO on 03/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalletApp/WalletApp/WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppApp.swift
+//  WalletApp
+//
+//  Created by MACBOOK PRO on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
